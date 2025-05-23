37382e0f59037f855d11fca8b7567a2f98cb7390
Nicolò Polazzi <nicolo.polazzi@gmail.com>
2025-05-23 13:26:03 +0200

    Initial camera script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Vector3 offset = new Vector3(0f, 1f, 0f);
+    [SerializeField] private float smoothSpeed = 5f;
+
+
+    // LateUpdte is called after all Update() calls are finished
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = new Vector3(smoothedPosition.x, smoothedPosition.y, -10f);
+    }
+}
