895a1711e788bb17f308fc386b83390081530553
Breezen <brizziemanuele@gmail.com>
2025-06-24 22:20:42 +0200

    Add Key bindings in Inspector
    Add Key bindings in Inspector

--- Assets/Scripts/Manager/PlayerCommandManager.cs
+++ Assets/Scripts/Manager/PlayerCommandManager.cs
@@ -3,18 +3,27 @@ using UnityEngine;
 
 public class PlayerCommandManager : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] CommandManager commandManager;
     [SerializeField] PlayerMovement playerMovement;
     [SerializeField] PlayerAttack playerAttack;
     [SerializeField] Transform playerTransform;
 
+    [Header("Key Bindings")]
+    [SerializeField] KeyCode moveLeftKey = KeyCode.A;
+    [SerializeField] KeyCode moveRightKey = KeyCode.D;
+    [SerializeField] KeyCode jumpKey = KeyCode.W;
+    [SerializeField] KeyCode meleeAttackKey = KeyCode.Q;
+    [SerializeField] KeyCode rangedAttackKey = KeyCode.F;
+    [SerializeField] KeyCode interactKey = KeyCode.E;
+
     void Start()
     {
-        commandManager.BindKey(KeyCode.A, new MoveLeftCommand(playerMovement));
-        commandManager.BindKey(KeyCode.D, new MoveRightCommand(playerMovement));
-        commandManager.BindKey(KeyCode.W, new JumpCommand(playerMovement));
-        commandManager.BindKey(KeyCode.Q, new MeleeAttackCommand(playerAttack));
-        commandManager.BindKey(KeyCode.F, new RangedAttackCommand(playerAttack));
-        commandManager.BindKey(KeyCode.E, new InteractCommand(playerTransform));
+        commandManager.BindKey(moveLeftKey, new MoveLeftCommand(playerMovement));
+        commandManager.BindKey(moveRightKey, new MoveRightCommand(playerMovement));
+        commandManager.BindKey(jumpKey, new JumpCommand(playerMovement));
+        commandManager.BindKey(meleeAttackKey, new MeleeAttackCommand(playerAttack));
+        commandManager.BindKey(rangedAttackKey, new RangedAttackCommand(playerAttack));
+        commandManager.BindKey(interactKey, new InteractCommand(playerTransform));
     }
 }
